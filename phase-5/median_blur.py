22d6f76a3852d2a9acbf25daf81e1e47ead958de
SwaroopDangal <swaroopdangal732@gmail.com>
2026-08-15 09:59:05 +0545

    phase 5 completed

---
+++ phase-5/median_blur.py
@@ -0,0 +1,11 @@
+import cv2
+#load
+image = cv2.imread("phase-5\\great-nature-waterfall-wide.webp")
+
+
+blurred = cv2.medianBlur(image,11)
+
+cv2.imshow("Original Image",image)
+cv2.imshow("Blurred Image",blurred)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
