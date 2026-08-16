dbc6ce470e7bb161ca15f2bc702936f6ec6cd137
SwaroopDangal <swaroopdangal732@gmail.com>
2026-08-16 09:40:04 +0545

    phase-8 completed

---
+++ phase-8/tempCodeRunnerFile.py
@@ -0,0 +1,23 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier("phase-8\\haarcascade_frontalface_default.xml")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray,1.1,5)
+    #1.1-> scale factor,5->min neighbors
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow("Webcam face detection",frame)
+
+    if cv2.waitKey(1) and 0xFF==ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
